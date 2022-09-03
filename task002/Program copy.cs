e9f21928dff0722b30d6c1f545e73d5f62ab8d0c
Tt <tatiankat77@gmail.com>
2022-09-04 02:15:21 +0300

    ДЗ задача 4

---
+++ task002/Program copy.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число, a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число, b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число, c");
+int c = int.Parse(Console.ReadLine());
+ 
+if ( a > b && a > c )
+{
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {a} ");
+}
+else if ( b > c)
+ {
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {b} ");
+ }
+ else
+ {
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {c} ");
+ }
+ 
