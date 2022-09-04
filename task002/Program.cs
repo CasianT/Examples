469eab4283e491a09e6d4b1634ddab1976e938df
Tt <tatiankat77@gmail.com>
2022-09-04 15:45:01 +0300

    ДЗ задача 4

--- task002/Program.cs
+++ task002/Program.cs
@@ -3,3 +3,23 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите первое число, a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число, b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число, c");
+int c = int.Parse(Console.ReadLine());
+ 
+if ( a > b && a > c )
+{
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {a} ");
+}
+else if ( b > c)
+ {
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {b} ");
+ }
+ else
+ {
+    Console.WriteLine($" a = {a}, b = {b}, c = {c} -> max = {c} ");
+ }
+ 
